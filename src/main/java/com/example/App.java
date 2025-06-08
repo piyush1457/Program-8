046272e7dc7078360152036a44f8e6242ef64a7c
Piyush Kumar Sodhi <piyushsodhi145@gmail.com>
2025-06-08 13:56:37 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int a=5;
+        int b-10;
+        int sum=a+b;
+        System.out.println( "The sun is"+sum );
     }
 }
