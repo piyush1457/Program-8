ac8a7eb0353d3300477f878ec0f876c3b1ee2439
Piyush Kumar Sodhi <piyushsodhi145@gmail.com>
2025-06-08 13:58:48 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         int a=5;
-        int b-10;
+        int b=10;
         int sum=a+b;
         System.out.println( "The sun is"+sum );
     }
